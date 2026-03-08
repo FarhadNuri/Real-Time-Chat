778b8f89f5c0a10f510403b2b299494d83ce32ca
Farhad Nuri <231006312@eastdelta.edu.bd>
2026-03-08 11:01:19 +0600

    Signup controller with JWT and Cookies

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,55 @@
-export async function login(req, res) {
-    res.send('Login endpoint');
-}
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokens } from "../utils/generateTokens.util.js";
 
 export async function signup(req, res) {
-    try {
-        const { email, fullname, password } = req.body;
-         
-    } catch (error) {
-        console.error('Error during signup:', error);
-        res.status(500).json({ message: 'Internal server error' });
+  const { email, fullname, password } = req.body;
+
+  if (!email || !fullname || !password) {
+    return res.status(400).json({ message: "Please fill in all fields" });
+  }
+
+  try {
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters long",
+      });
     }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already in use",
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = new User({
+      email,
+      fullname,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    await generateTokens(user._id, res);
+
+    res.status(201).json({
+      message: "User created successfully",
+      id: user._id,
+      email: user.email,
+      fullname: user.fullname,
+      profilepic: user.profilepic,
+    });
+
+  } catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 }
